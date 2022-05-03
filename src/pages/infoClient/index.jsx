5737c02cf2d5cc1a47c10c693d420dd26bdba66b
Gabriel <gabrielbvlkenzie@gmail.com>
2022-05-03 03:11:18 -0300

    feat: styling a single customer request

--- src/pages/infoClient/index.jsx
+++ src/pages/infoClient/index.jsx
@@ -25,7 +25,29 @@ function InfoClient({}) {
         }
     };
 
-    return <div>{item.TECL_NOME}</div>;
+    return (
+        <div className="container_client">
+            <h1>Informações do cliente {item.TECL_ID}</h1>
+            <div className="client_infos">
+                <div className="infos" id="info_name">
+                    <h2>Cliente</h2>
+                    <h3> {item.TECL_NOME} </h3>
+                </div>
+
+                <div className="infos" id="info_address">
+                    <h2>Endereço</h2>
+                    <h3>Rua: {item.TECL_ENDERECO}</h3>
+                    <h3>Cidade: {item.TECL_CIDADE}</h3>
+                    <h3>Estado: {item.TECL_UF}</h3>
+                </div>
+
+                <div className="infos" id="info_contact">
+                    <h2>Contato</h2>
+                    <h3>Número para contato: {item.TECL_TELEFONE}</h3>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default InfoClient;
